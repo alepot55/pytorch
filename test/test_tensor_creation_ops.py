fffa35f715a2bd139fc2d9127cc7b5c5d4cffe8b
KarhouTam <karhou.tam@outlook.com>
2026-09-20 01:41:28 +0000

    [Test] Refactor `TestLikeTensorCreation` class
    - Move CPU only test case to new device specific class `TestLikeTensorCreationCpuOnly`
    - Add hw label to `TestBufferProtocol` and `TestFromBlob`
    
    ghstack-source-id: f4542376e82022895dad20ae88655e99228d78b5
    Pull-Request: https://github.com/pytorch/pytorch/pull/193715

--- test/test_tensor_creation_ops.py
+++ test/test_tensor_creation_ops.py
@@ -3779,6 +3779,7 @@ class TestRandomTensorCreationCpuOnly(TestCase):
 
 # Class for testing *like ops, like torch.ones_like
 class TestLikeTensorCreation(TestCase):
+    hw_classification = HardwareClassification.ACCELERATOR
     exact_dtype = True
 
     # TODO: this test should be updated
@@ -3792,16 +3793,6 @@ class TestLikeTensorCreation(TestCase):
         expected = torch.tensor([True, True], device=device, dtype=torch.bool)
         res1 = torch.ones_like(expected)
         self.assertEqual(res1, expected)
-
-    # TODO: this test should be updated
-    @onlyCPU
-    def test_empty_like(self, device):
-        x = torch.autograd.Variable(torch.tensor([]))
-        y = torch.autograd.Variable(torch.randn(4, 4))
-        z = torch.autograd.Variable(torch.IntTensor([1, 2, 3]))
-        for a in (x, y, z):
-            self.assertEqual(torch.empty_like(a).shape, a.shape)
-            self.assertEqualTypeString(torch.empty_like(a), a)
 
     def test_zeros_like(self, device):
         expected = torch.zeros((100, 100,), device=device)
@@ -3964,6 +3955,19 @@ class TestLikeTensorCreation(TestCase):
         self.assertNotEqual(tensor0, tensor1)
 
 
+class TestLikeTensorCreationCpuOnly(TestCase):
+    hw_classification = HardwareClassification.CPU
+
+    # TODO: this test should be updated
+    def test_empty_like(self, device):
+        x = torch.autograd.Variable(torch.tensor([]))
+        y = torch.autograd.Variable(torch.randn(4, 4))
+        z = torch.autograd.Variable(torch.IntTensor([1, 2, 3]))
+        for a in (x, y, z):
+            self.assertEqual(torch.empty_like(a).shape, a.shape)
+            self.assertEqualTypeString(torch.empty_like(a), a)
+
+
 # Tests for the `frombuffer` function (only work on CPU):
 #   Constructs tensors from Python objects that implement the buffer protocol,
 #   without copying data.
@@ -3976,7 +3980,10 @@ def may_require_grad(dtype):
 def get_dtype_size(dtype):
     return int(torch.empty((), dtype=dtype).element_size())
 
+
 class TestBufferProtocol(TestCase):
+    hw_classification = HardwareClassification.CPU
+
     def _run_test(self, shape, dtype, count=-1, first=0, offset=None, **kwargs):
         numpy_dtype = torch_to_numpy_dtype_dict[dtype]
 
@@ -4132,6 +4139,8 @@ class TestBufferProtocol(TestCase):
         self.assertSequenceEqual(tensor, [255, 255])
 
 class TestFromBlob(TestCase):
+    hw_classification = HardwareClassification.CPU
+
     def _make_data(self, dtype, numel):
         numpy_dtype = torch_to_numpy_dtype_dict[dtype]
         arr = np.arange(1, numel + 1, dtype=numpy_dtype)
@@ -4533,6 +4542,7 @@ instantiate_device_type_tests(TestTensorCreationCudaOnly, globals(), only_for="c
 instantiate_device_type_tests(TestRandomTensorCreation, globals(), allow_xpu=True)
 instantiate_device_type_tests(TestRandomTensorCreationCpuOnly, globals(), only_for="cpu")
 instantiate_device_type_tests(TestLikeTensorCreation, globals(), allow_xpu=True)
+instantiate_device_type_tests(TestLikeTensorCreationCpuOnly, globals(), only_for="cpu")
 instantiate_device_type_tests(TestBufferProtocol, globals(), only_for="cpu")
 instantiate_device_type_tests(TestFromBlob, globals(), only_for="cpu")
 instantiate_device_type_tests(TestAsArray, globals(), allow_xpu=True)
